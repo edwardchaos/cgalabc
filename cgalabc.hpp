4842c30b70268f16e4bb3002f5c858c9698c4a4c
Edward Chao <edward_888@hotmail.com>
2020-12-09 15:47:01 -0500

    Initial file setup

---
+++ cgalabc.hpp
@@ -0,0 +1,6 @@
+#ifndef CGALABC_LIBRARY_H
+#define CGALABC_LIBRARY_H
+
+void hello();
+
+#endif //CGALABC_LIBRARY_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "cgalabc.hpp"
+
+int main(){
+
+  hello();
+  return 0;
+}
