b377e9199e0337362495b3d66346b9ff5a92b9d6
Edward Chao <edward_888@hotmail.com>
2020-12-10 13:14:30 -0500

    Create tests for line2d

--- test/TestLine2d.cpp
+++ test/TestLine2d.cpp
@@ -2,9 +2,59 @@
 
 #include "cgalabc.hpp"
 
-TEST(case1, function1){
-  cg::Point2d mypoint(2,3);
-  EXPECT_TRUE(true);
-  mypoint.print();
-  ASSERT_TRUE(false);
+TEST(Testlines, create_lines){
+  // Create line given 1 point
+  cg::Point2d pt1(2,3);
+  cg::Line2d l1(pt1);
+
+  // Test line equation
+  ASSERT_EQ(l1.getA(), -3.0/2.0);
+  ASSERT_EQ(l1.getB(), 1);
+  ASSERT_EQ(l1.getC(), 0);
+
+  // Create line given 2 points
+  cg::Point2d pt2(-2, -1);
+  cg::Point2d pt3(10, 6);
+  cg::Line2d l2(pt2, pt3);
+  // Test line equation
+  ASSERT_EQ(l2.getA(), -7.0/12.0);
+  ASSERT_EQ(l2.getB(), 1);
+  ASSERT_EQ(l2.getC(), -1.0/6.0);
+
+  // Try creating line with 2 identical points,
+  try{
+    cg::Line2d(pt1,pt1));
+  }catch(std::Exception &e){
+    EXPECT_EQ(e.what(), std::string("Identical points used to create Line2d."));
+  }
+
+// Expect runtime error? or what should I do?
+
+  // Create horizontal line with 1 point
+  cg::Line2d h_line({1,0});
+
+  // Check line equation
+  ASSERT_EQ(h_line.getA(), 0);
+  ASSERT_EQ(h_line.getB(), 1);
+  ASSERT_EQ(h_line.getC(), 0);
+
+  // Create horizontal line with 2 points
+  cg::Point2d pt4(-10,4);
+  cg::Point2d pt5(4,4);
+  cg::Line2d h_line2(pt4,pt5);
+
+  // check line equation
+  ASSERT_EQ(h_line2.getA(), 0);
+  ASSERT_EQ(h_line2.getB(), 1);
+  ASSERT_EQ(h_line2.getC(), -4);
+
+  // Create vertical line
+  cg::Point2d pt6(-32.5, 8);
+  cg::Point2d pt7(-32.5, -99);
+  cg::Line2d v_line(pt6, pt7);
+
+  // Check line equation
+  ASSERT_EQ(v_line.getA(), 1);
+  ASSERT_EQ(v_line.getB(), 0);
+  ASSERT_EQ(v_line.getC(), 32.5);
 }
