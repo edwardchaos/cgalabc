094d093e2ac056164d15e21f32954212e407a61d
Edward Chao <edward_888@hotmail.com>
2020-12-11 14:57:57 -0500

    Add tests for point to infinite line and segment

--- test/TestAlgo2d.cpp
+++ test/TestAlgo2d.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include <cmath>
 #include "type.h"
 #include "Algo2d.h"
 
@@ -26,6 +27,7 @@ TEST(Algo, basic_properties){
   ASSERT_NEAR(intersect45->x(), 1, cg::EPS);
   ASSERT_NEAR(intersect45->y(), 1, cg::EPS);
 
+  ASSERT_FALSE(cg::intersects(l1,l2));
   ASSERT_EQ(cg::intersectPoint(l1,l2), nullptr);
   cg::Line2d l6({2,-1},{3,-2});
   ASSERT_FALSE(cg::intersects(l1,l6));
@@ -33,13 +35,38 @@ TEST(Algo, basic_properties){
 
 TEST(Algo, basic_algos){
   cg::Point2d pt1(5,5);
+  cg::Point2d pt2(1,5);
+  cg::Point2d pt3(1,0);
+  cg::Point2d pt4(0.5,0);
+  cg::Point2d pt5(0,-5);
+
   cg::Line2d l1({1,0});
 
   // Shortest distance of point to line
-  auto dist = distancePointToLine(pt1, l1);
+  auto dist1 = distancePointToLine(pt1, l1);
+  auto dist2 = distancePointToLine(pt2, l1);
+  auto dist3 = distancePointToLine(pt3, l1);
+  auto dist4 = distancePointToLine(pt4, l1);
+  auto dist5 = distancePointToLine(pt5, l1);
+
+  ASSERT_NEAR(dist1, 5.0, cg::EPS);
+  ASSERT_NEAR(dist2, 5.0, cg::EPS);
+  ASSERT_NEAR(dist3, 0.0, cg::EPS);
+  ASSERT_NEAR(dist4, 0.0, cg::EPS);
+  ASSERT_NEAR(dist5, 5.0, cg::EPS);
 
   // Shortest distance of point to line segment
-  auto dist2 = distancePointToLineSegment(pt1, l1);
+  auto dls1 = distancePointToLineSegment(pt1, l1);
+  auto dls2 = distancePointToLineSegment(pt2, l1);
+  auto dls3 = distancePointToLineSegment(pt3, l1);
+  auto dls4 = distancePointToLineSegment(pt4, l1);
+  auto dls5 = distancePointToLineSegment(pt5, l1);
+
+  ASSERT_NEAR(dist1, sqrt(26), cg::EPS);
+  ASSERT_NEAR(dist2, 5.0, cg::EPS);
+  ASSERT_NEAR(dist3, 0.0, cg::EPS);
+  ASSERT_NEAR(dist4, 0.0, cg::EPS);
+  ASSERT_NEAR(dist5, 5.0, cg::EPS);
 
   // Check polygon convex
   // Area of polygon (shoelace method)
