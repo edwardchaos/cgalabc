49697535c9232e79eebc7a92cf880b12c2107d65
Edward Chao <edward_888@hotmail.com>
2020-12-11 20:58:47 -0500

    Add hash so unordered containers can be used

--- src/Point2d.h
+++ src/Point2d.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstddef>
+#include <functional>
+
 namespace cg {
 
 // Small value for comparing double floating equality
@@ -18,11 +21,21 @@ class Point2d {
   bool operator==(const Point2d &other) const;
   bool operator!=(const Point2d &other) const;
 
+
   double dist(const Point2d &other) const;
   void print() const;
 
  private:
   double x_, y_;
+};
+
+struct hashPoint2d{
+  std::size_t operator()(Point2d const&pt)const{
+    std::size_t h = 17;
+    h = h * 31 + std::hash<double>()(pt.x());
+    h = h * 31 + std::hash<double>()(pt.y());
+    return h;
+  }
 };
 
 } // namespace cg
