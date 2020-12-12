8c09afaea578a9b2fba06c434df033bb4f05916c
Edward Chao <edward_888@hotmail.com>
2020-12-11 20:59:18 -0500

    add test for minimum 3 unique points in polygon

--- test/TestPolygon2d.cpp
+++ test/TestPolygon2d.cpp
@@ -6,41 +6,56 @@
 TEST(Polygon, create_polygon){
   try {
     // Create 2 valid polygons
-    cg::Polygon poly{{0, 0}, {10, 0}, {10, 10}, {0, 10}};
+    cg::Polygon{{0, 0}, {10, 0}, {10, 10}, {0, 10}};
     std::vector<cg::Point2d> pointset2{{-5, -5}, {5, -5}, {5, 5}};
-    cg::Polygon poly2(pointset2);
+    cg::Polygon{pointset2};
   }catch(...){FAIL() << "No exception should be thrown.";}
 
   // Polygon with only 2 unique points
   try {
-    cg::Polygon poly1_2points{{0, 0}, {0, 1}, {0, 0}};
+    cg::Polygon{{0, 0}, {0, 1}, {0, 0}};
+    FAIL() << "Should throw invalid_argument";
   }catch(std::invalid_argument &e) {
     ASSERT_STREQ(e.what(),
-                 "Cannot create a 2d polygon with 2 unique points, requires 3 "
-                 "minimum");
+                 "Cannot create polygon; Requires at least 3 unique vertices");
   }catch(...){FAIL() << "Incorrect exception thrown";}
 
   // Polygon with 2 unique points, excluding the last "closing the loop point"
   try{
-    cg::Polygon poly2_2points{{0,0},{0,1}};
+    cg::Polygon{{0,0},{0,1}};
+    FAIL() << "Should throw invalid_argument";
   }catch(std::invalid_argument &e) {
     ASSERT_STREQ(e.what(),
-               "Cannot create a 2d polygon with 2 unique points, requires 3 "
-               "minimum");
+                 "Cannot create polygon; Requires at least 3 unique vertices");
   }catch(...) {FAIL() << "Incorrect exception thrown";}
 
+  // Polygon with repeated intermediate vertices
+  try{
+    cg::Polygon{{0,0},{1,1},{0,1},{-1,0},{1,1}};
+    FAIL() << "Should throw invalid argument";
+  }catch(std::invalid_argument &e){
+  }catch(...){
+    FAIL() << "Expected invalid argument exception";
+  }
+
   // Polygon with 2 overlapping edges
-  try{cg::Polygon pg{{0,0},{1,0},{1,1},{1,0}};}
+  try{
+    cg::Polygon{{0,0},{1,0},{1,1},{1,0}};
+    FAIL() << "Should throw invalid argument";
+  }
   catch(std::invalid_argument &e){
     ASSERT_STREQ(e.what(),
-                 "Mon dieu! Cannot create 2d polygon; 2 edges are coincident.");
+                 "Mon dieu! Cannot create polygon; 2 edges are coincident.");
   }
   catch(...){FAIL() << "Incorrect exception thrown";}
 
   // Pair of edges in polygon intersect
-  try{cg::Polygon pg2{{0,0}, {1,0}, {1,1}, {0,-1}};}
+  try {
+    cg::Polygon{{0, 0}, {1, 0}, {1, 1}, {0, -1}};
+    FAIL() << "Should throw invalid argument";
+  }
   catch(std::invalid_argument &e){
-    ASSERT_STREQ(e.what(), "Cannot create 2d polygon; 2 edges intersect.");
+    ASSERT_STREQ(e.what(), "Cannot create polygon; 2 edges intersect.");
   }
   catch(...){FAIL() << "Incorrect exception thrown";}
 }
