bc2f4fc74081b4edab37cd2b8cd7c4807b9407ee
Edward Chao <edward_888@hotmail.com>
2020-12-16 19:15:09 -0500

    Link libraries and run olc demo

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 The preschool version of CGAL
 
 Written as a learning exercise about computational geometry.
+
+For visualization, olc Pixel game engine is used.
+There are some dependencies for that:
+
+* glew: Modern OpenGL interface.
+  * sudo apt install libglew-dev
+* libpng: Portable Network Graphics library to load .png-files.
+  * sudo apt install libpng-dev
+* X11: Basic graphics interface for Linux to create a windows.
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
-#include "type.h"
+#define OLC_PGE_APPLICATION
+
+#include "olcPixelGameEngine.h"
+#include "Algo2d.h"
+
+class DisplayEngine : public olc::PixelGameEngine{
+ public:
+  bool OnUserCreate() override{
+    return true;
+  }
+  bool OnUserUpdate(float fElapsedTime) override{
+    // called once per frame
+    for (int x = 0; x < ScreenWidth(); x++)
+      for (int y = 0; y < ScreenHeight(); y++)
+        Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));
+    return true;
+  }
+};
 
 int main(){
-  cg::Point2d mypoint(1,2);
-
-  mypoint.print();
-
-  /* Potential use case
-   * vec dot vec
-   * vec cross vec
-   * shortest distance of point to line
-   * shortest distance of point to vector
-   *
-   */
+  DisplayEngine engine;
+  if(!engine.Construct(640, 480, 1,1)) return 0;
+  engine.Start();
   return 0;
 }
