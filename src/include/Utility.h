106dbb6c38352c205c3fb745a82ca6d6e91cfdf4
Edward Chao <edward_888@hotmail.com>
2020-12-22 18:29:07 -0500

    Move debug cube to utility

--- src/include/Utility.h
+++ src/include/Utility.h
@@ -59,4 +59,35 @@ Mesh_ptr loadOBJ(std::string path_to_obj){
 
   return mesh;
   };
+
+Mesh_ptr cube(){
+  Mesh_ptr cube = std::make_shared<Mesh>();
+
+  // create cube
+  // South face
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(1,1,0),Vector3d(0,1,0));
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(1,0,0),Vector3d(1,1,0));
+
+  // East face
+  cube->tris.emplace_back(Vector3d(1,0,0),Vector3d(1,1,-1),Vector3d(1,1,0));
+  cube->tris.emplace_back(Vector3d(1,0,0),Vector3d(1,0,-1),Vector3d(1,1,-1));
+
+  // West face
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(0,1,0),Vector3d(0,1,-1));
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(0,1,-1),Vector3d(0,0,-1));
+
+  // North face
+  cube->tris.emplace_back(Vector3d(0,0,-1),Vector3d(0,1,-1),Vector3d(1,1,-1));
+  cube->tris.emplace_back(Vector3d(0,0,-1),Vector3d(1,1,-1),Vector3d(1,0,-1));
+
+  // Top face
+  cube->tris.emplace_back(Vector3d(0,1,0),Vector3d(1,1,0),Vector3d(1,1,-1));
+  cube->tris.emplace_back(Vector3d(0,1,0),Vector3d(1,1,-1),Vector3d(0,1,-1));
+
+  // Bottom face
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(0,0,-1),Vector3d(1,0,-1));
+  cube->tris.emplace_back(Vector3d(0,0,0),Vector3d(1,0,-1),Vector3d(1,0,0));
+
+  return cube;
+}
 } // namespace cg
