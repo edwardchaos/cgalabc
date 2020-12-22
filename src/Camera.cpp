722109558f671ea0df4dba1da92a71631260642b
Edward Chao <edward_888@hotmail.com>
2020-12-21 19:26:52 -0500

    Implement projection matrix

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,13 +1,35 @@
 #include "Camera.h"
 
-namespace cg {
+#include <cmath>
 
+namespace cg {
 
 Camera::Camera(double aspect_ratio, double vertical_fov_rad, double near,
                double far): ar_(aspect_ratio), vertical_fov_
-               (vertical_fov_rad), near_(near), far_(far){
+               (vertical_fov_rad), near_plane_dist_(near), far_plane_dist_(far){
 
+  init();
 }
+
+void Camera::init(){
+  pose_world_ = Eigen::Matrix4d::Ones();
+  constructProjectionMatrix();
+}
+
+void Camera::constructProjectionMatrix(){
+  projection_mat_ = Eigen::Matrix4d::Zero();
+  // cos(x)/sin(x) numerically stable version of cot(x)
+  double f = std::cos(vertical_fov_/2.0) / std::sin(vertical_fov_/2.0);
+
+  projection_mat_(0,0) = ar_*f;
+  projection_mat_(1,1) = f;
+  projection_mat_(2,2) = (near_plane_dist_+far_plane_dist_)
+      /(far_plane_dist_-near_plane_dist_);
+  projection_mat_(3,2) = (2*far_plane_dist_*near_plane_dist_)
+      /(far_plane_dist_-near_plane_dist_);
+  projection_mat_(2,3) = -1;
+}
+
 
 
 } // namespace cg
--- src/Camera.h
+++ src/Camera.h
@@ -1,18 +1,40 @@
 #pragma once
 
+#include <Eigen/Dense>
+
+using Eigen::Matrix4d;
+using Eigen::Vector4d;
+
 namespace cg{
 
+/*
+ * Camera frustum that can transform the 3d world to a 2d image plane by
+ * perspective transform.
+ */
 class Camera{
+  /*
+   * Camera initialized with pose at the origin and no rotation.
+   *
+   * Aspect ratio: height/width
+   * Vertical field of view in radians
+   * Near plane distance in world units
+   * Far plane distance in world units
+   */
   Camera(double aspect_ratio, double vertical_fov_rad, double near, double
   far);
 
  private:
-  double ar_, vertical_fov_, near_, far_;
+  // Distance is in units of the world
+  double ar_, vertical_fov_, near_plane_dist_, far_plane_dist_;
 
   // Pose in world frame
+  Matrix4d pose_world_;
 
   // Projection matrix
+  Matrix4d projection_mat_;
 
+  void init();
+  void constructProjectionMatrix();
 };
 
 } // namespace cg
