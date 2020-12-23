08c37939d30bc1cdb8fad6729d90673d75a00cf7
Edward Chao <edward_888@hotmail.com>
2020-12-23 18:42:27 -0500

    Make pose public

--- src/Pose.cpp
+++ src/Pose.cpp
@@ -5,13 +5,4 @@ namespace cg{
 Pose::Pose(Eigen::Vector4d position, Eigen::Matrix4d orientation):
 orientation_(std::move(orientation)), position_(std::move(position)){}
 
-Eigen::Vector4d Pose::position()const{
-  return position_;
-}
-
-Eigen::Matrix4d Pose::orientation()const{
-  return orientation_;
-
-}
-
 } // namespace cg
--- src/include/Pose.h
+++ src/include/Pose.h
@@ -11,13 +11,11 @@ class Pose{
  public:
   Pose()=default;
   Pose(Eigen::Vector4d position, Eigen::Matrix4d orientation);
-  Eigen::Vector4d position()const;
-  Eigen::Matrix4d orientation()const;
 
- private:
   // Homogenous coordinates for convenience
   Eigen::Matrix4d orientation_ = Eigen::Matrix4d::Identity();
   Eigen::Vector4d position_ = Eigen::Vector4d::Identity();
+ private:
 };
 
 } // namespace cg
