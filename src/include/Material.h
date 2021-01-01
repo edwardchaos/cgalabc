f649f13a843359dd82f9ddfc7ef9817e9dd4fc1a
Edward Chao <edward_888@hotmail.com>
2021-01-01 04:15:40 -0500

    Add comments

--- src/include/Material.h
+++ src/include/Material.h
@@ -5,10 +5,12 @@
 namespace cg{
 
 struct Material{
-  double ka[3]={1,1,1};
-  double kd[3]={1,1,1};
-  double ks[3]={1,1,1};
-  double ke[3]={1,1,1};
+  double ka[3]={1,1,1}; // material ambient reflectivity
+  double kd[3]={1,1,1}; // material diffusion reflectivity
+  double ks[3]={1,1,1}; // material specular reflectivity
+
+  // Material emission (base brightness in the absence of a light source)
+  double ke[3]={0.02,0.02,0.02};
   double Ns; // Glossiness exponent
   std::shared_ptr<olc::Sprite> texture = nullptr;
 
