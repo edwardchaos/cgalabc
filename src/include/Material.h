8a3a1ef2a313e0e1e5ef103171ce1ba265af79be
Edward Chao <edward_888@hotmail.com>
2021-01-01 04:11:20 -0500

    Material struct members

--- src/include/Material.h
+++ src/include/Material.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#include "olcPixelGameEngine.h"
+
 namespace cg{
 
+struct Material{
+  double ka[3]={1,1,1};
+  double kd[3]={1,1,1};
+  double ks[3]={1,1,1};
+  double ke[3]={1,1,1};
+  double Ns; // Glossiness exponent
+  std::shared_ptr<olc::Sprite> texture = nullptr;
 
+  Material()=default;
+
+};
 
 } // namespace cg
