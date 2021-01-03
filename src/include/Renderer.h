b4ae5444e9fc173c6f9777761e9800bd23847b8a
Edward Chao <edward_888@hotmail.com>
2021-01-02 19:51:07 -0500

    Add main shade function prototype

--- src/include/Renderer.h
+++ src/include/Renderer.h
@@ -39,6 +39,48 @@ class Renderer : public olc::PGEX{
       const Triangle& triangle,
       const std::unordered_map<std::string, Light_ptr> &lights);
 
+  /*
+   * Main shading operations.
+   *
+   * All Vector3d arguments associate to their r,g,b counterpart
+   * eg material_ambience[0]: red component of material ambience
+   * material_ambience[1]: green component
+   * material_ambience[2]: blue component
+   *
+   * Args:
+   * material_ambience: ambience reflectivity of material [0,1]
+   * material_diffuse: diffusion reflectivity of material [0,1]
+   * In the absence of external texture, material diffuse value mainly
+   * discerns color
+   *
+   * material_specular: specular reflectivity of material [0,1]
+   * surface_normal: surface normal unit vector
+   * point_to_light: unit vector direction from surface point to light source
+   * glossiness_exponent: affects illusion of glossiness [0,1000]
+   * material_emittance: emission of color in absence of light source
+   * light_ambience: ambience reflection of light source [0,1] (fancy light?)
+   * light_diffuse: diffusion reflection of light source [0,1]
+   * light_specular: specular reflection of light source [0,1]
+   * texture: color from texture
+   *
+   * equation:
+   * I=(m_amb*l_amb, m_dif*l_dif(n.dot(l)) + m_emit)*texture_color
+   *  +m_spec*l_spec(n.dot(halfway_cam_to_light))
+   *
+   *  Note: blinn-phong shading (halfway dir between camera and light)
+   */
+  Vector3d shade(const Vector3d &material_ambience,
+                 const Vector3d &material_diffuse,
+                 const Vector3d &material_specular,
+                 const Vector3d &surface_normal,
+                 const Vector3d &point_to_light,
+                 double glossiness_exponent,
+                 const Vector3d &material_emittance = Vector3d(0.02,0.02,0.02),
+                 const Vector3d &light_ambience = Vector3d(1,1,1),
+                 const Vector3d &light_diffuse = Vector3d(1,1,1),
+                 const Vector3d &light_specular = Vector3d(1,1,1),
+                 const Vector3d &texture = Vector3d(1,1,1));
+
  private:
   std::vector<std::vector<double>> depth_buffer;
 };
