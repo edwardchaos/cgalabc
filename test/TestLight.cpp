a7330688f61670e09c3ea085e7c7e960eec57bf8
Edward Chao <edward_888@hotmail.com>
2021-01-04 23:00:17 -0500

    Add test for clone and transform

--- test/TestLight.cpp
+++ test/TestLight.cpp
@@ -7,10 +7,8 @@
 
 TEST(Light, getDirection){
   std::vector<cg::Light_ptr> lights;
-
   // Directional light pointed in the 1,1,1 direction
   lights.push_back(std::make_shared<cg::DirectionLight>(Vector3d(1,1,1)));
-
   // Point light positioned at 1,1,1
   lights.push_back(std::make_shared<cg::PointLight>(Vector4d(1,1,1,1)));
 
@@ -28,6 +26,34 @@ TEST(Light, getDirection){
 }
 
 TEST(Light, clone_transform){
+  std::vector<cg::Light_ptr> lights;
+  // Directional light pointed in the 1,1,1 direction
+  lights.push_back(std::make_shared<cg::DirectionLight>(Vector3d(1,1,1)));
+  // Point light positioned at 1,1,1
+  lights.push_back(std::make_shared<cg::PointLight>(Vector4d(1,1,0,1)));
 
+  auto cloned_dir_light = lights[0]->clone();
+  auto cloned_pt_light = lights[1]->clone();
+
+  Matrix4d tf = Matrix4d::Identity();
+  // Rotation in y axis by 180 deg
+  tf(0,0) = -1;
+  tf(2,2) = -1;
+  // Translation in z axis by 5 units
+  tf(2,3) = 5;
+
+  cloned_dir_light->transform(tf);
+  cloned_pt_light->transform(tf);
+
+  // Original light directions
+  auto directional_dir = lights[0]->getDirection(Vector3d(0,0,0));
+  auto point_dir = lights[1]->getDirection(Vector3d(0,0,0));
+  // Cloned, transformed light directions
+  auto cloned_directional_dir = cloned_dir_light->getDirection(Vector3d(0,0,0));
+  auto cloned_pt_dir = cloned_pt_light->getDirection(Vector3d(0,0,0));
+
+  ASSERT_TRUE(directional_dir.isApprox(Vector3d(1,1,1).normalized()));
+  ASSERT_TRUE(point_dir.isApprox(Vector3d(-1,-1,0).normalized()));
+  ASSERT_TRUE(cloned_directional_dir.isApprox(Vector3d(-1,1,-1).normalized()));
+  ASSERT_TRUE(cloned_pt_dir.isApprox(Vector3d(1,-1,-5).normalized()));
 }
-
