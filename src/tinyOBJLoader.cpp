1f6c0574b62e4cf80545e0a15b54a69e6b110ba4
Edward Chao <edward_888@hotmail.com>
2021-01-05 18:21:02 -0500

    Add tinyobjloader single header and cpp to project

---
+++ src/tinyOBJLoader.cpp
@@ -0,0 +1,7 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+
+// Everything becomes double rather than default float
+// (described in tinyobjloader readme)
+#define TINYOBJLOADER_USE_DOUBLE
+
+#include "tiny_obj_loader.h"
